921498c7dee2684fad695fb8bbc8c6ac9ee5cd96
NandhuReddy007 <spandhanreddy1001@gmail.com>
2025-09-29 11:35:21 +0530

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,54 +1,55 @@
-`include "package.svh"
-`include "async_fifo.v"
+`include "package_tb.sv"
 `include "interface.sv"
+`include "test.sv"
+
 module tb_top();
-  import uvm_pkg ::*;
-  import tb_pkg ::*;
-  bit pclk, preset;
-  intf if_f(.pclk(pclk), .preset (preset) ) ;
-  apb dut(.pclk(pclk), .preset(preset) , .pwdata(if_f.pwdata), .pwrite(if_f.pwrite), .paddr(if_f.paddr), .psel(if_f.psel), .penable(if_f.penable), .prdata(if_f.prdata) , .pready(if_f.pready), .pslverr(if_f.pslverr), .pstrb(if_f.pstrb));
-  always #5 pclk =~ pclk;
+  bit w_clk, r_clk, w_rst, r_rst;
+
+  fifo_if inf(w_clk, r_clk, w_rst, r_rst);
   
-  initial begin
-    uvm_config_db#(virtual intf)::set(uvm_root::get(),"*", "if_f",if_f);
-    preset=1;
-    pclk=0;
-    #15 preset=0;
-  end
+  test t1(inf);
+
+  fifo_16_8 dut(
+    .w_clk(inf.w_clk), .r_clk(inf.r_clk),
+    .w_rst(inf.w_rst), .r_rst(inf.r_rst),
+    .r_en(inf.r_en), .w_en(inf.w_en),
+    .d_in(inf.d_in), .full(inf.full),
+    .empty(inf.empty), .d_out(inf.d_out)
+  );
+
   
-  property p1;
-    @(posedge pclk) disable iff(preset)
-    (( if_f.psel && !if_f.penable)) ##[1:3] (if_f.psel && if_f.penable) |=> if_f.pready;
+  property forced_write_when_full;
+    @(posedge w_clk) disable iff(r_rst||w_rst)
+    (inf.full && inf.w_en) |-> $stable(inf.d_in);
   endproperty
-  assert property(p1)begin
-    `uvm_info("ASSERTION PASSED", $sformatf("pl ASSERTION PASSED") , UVM_HIGH) ;
-  end
-  else begin
-    `uvm_info("ASSERTION1 FAILED", $sformatf("pl ASSERTION FAILED") ,UVM_HIGH);
-  end
+  
+  property forced_read_when_empty;
+    @(posedge r_clk) disable iff(r_rst||w_rst)
+    (inf.empty && inf.r_en) |-> $stable(inf.d_out);
+  endproperty
+  
+  a1: assert property(forced_write_when_full) begin $display("ASSERTION1 PASSED"); end
+    else $display("ASSERTION1 FAILED");
+  
+  a2: assert property( forced_read_when_empty) begin $display("ASSERTION2 PASSED"); end
+    else $display("ASSERTION2 FAILED");
     
-  property p2;
-    @(posedge pclk) disable iff(preset)
-    $rose(if_f.penable) |-> $stable(if_f.paddr) ##0 $stable(if_f.pwdata || if_f.prdata) ##0 $stable(if_f.pwrite)##0 $stable(if_f.psel);
-  endproperty
-  assert property(p2)begin
-    `uvm_info("ASSERTION2", $sformatf("p2 ASSERTION PASSED"), UVM_HIGH);
-  end
-  else
-    $error("data is not stable");
-    
-  property p3;
-    @(posedge pclk) disable iff(preset)
-  	(if_f.psel && if_f.penable && !if_f.pwrite) |-> $isunknown(if_f.prdata) == 0;
-  endproperty
-  assert property(p3)begin
-    `uvm_info("ASSERTION", $sformatf("p3 ASSERTION PASSED") , UVM_HIGH);
-  end
-  else
-    $error("unknown prdata");
 
   initial begin
-    run_test("basetest"); 
+    w_clk = 0;
+    r_clk = 0;
+    w_rst = 1;
+    r_rst = 1;
+    #10 w_rst = 0;
+    #10 r_rst = 0;
+    
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_top);
+   
   end
+
+  always #5 w_clk = ~w_clk;
+  always #10 r_clk = ~r_clk;
 
 endmodule
+
