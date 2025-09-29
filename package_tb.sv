2371a43ec2662836e7122a14bc2799d8cce93730
NandhuReddy007 <spandhanreddy1001@gmail.com>
2025-09-29 11:33:06 +0530

    Update and rename package.svh to package_tb.sv

---
+++ package_tb.sv
@@ -0,0 +1,12 @@
+package pkg;
+
+  int no_txn=20;
+  `include "transactor.sv"
+  `include "generator.sv"
+  `include "driver.sv"
+  `include "receiver.sv"
+  `include "scoreboard.sv"
+  `include "environment.sv"
+
+
+endpackage
