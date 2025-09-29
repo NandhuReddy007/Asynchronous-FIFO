381e1b16f4f136da28719ea1bcb0f2d468006180
NandhuReddy007 <spandhanreddy1001@gmail.com>
2025-09-29 11:33:55 +0530

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,30 +1,9 @@
-interface intf (input logic pclk,preset);
-  logic [31:0] pwdata;
-  logic [7:0] paddr;
-  logic psel, penable,pwrite, pready, pslverr;
-  logic [31:0] prdata;
-  logic [3:0] pstrb;
-  logic reset;
+interface fifo_if(input logic w_clk, r_clk, w_rst, r_rst);
   
-  clocking driver_cb @(posedge pclk);
-    output pwdata;
-    output psel,penable, pwrite;
-    output paddr;
-    output pstrb;
-    input prdata, pslverr, pready;
-  endclocking
+  logic w_en;
+  logic r_en;
+  logic [7:0]d_in;
+  logic [7:0]d_out;
+  logic empty, full;
   
-  clocking monitor_cb @(posedge pclk);
-    input reset;
-    input pwdata;
-    input psel,penable,pwrite;
-    input paddr;
-    input pstrb;
-    input prdata,pslverr,pready;
-  endclocking
-  
-  modport dr(clocking driver_cb, input pclk, preset) ;
-  modport mo(clocking monitor_cb, input pclk, preset);
-
 endinterface
-    
